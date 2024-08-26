c0c63dbd5848931c13a1288d89b9227ccc0cd5f0
Kristian Underdal <kriu@stud.ntnu.no>
2024-08-26 16:09:08 +0200

    mandagsjobbing

--- linear_regression.py
+++ linear_regression.py
@@ -5,7 +5,10 @@ class LinearRegression():
     def __init__(self):
         # NOTE: Feel free to add any hyperparameters 
         # (with defaults) as you see fit
-        pass
+        self.weights = None
+        self.bias = None
+        self.epochs = 100
+        self.lr = 0.01
         
     def fit(self, X, y):
         """
@@ -16,8 +19,18 @@ class LinearRegression():
                 m rows (#samples) and n columns (#features)
             y (array<m>): a vector of floats
         """
-        # TODO: Implement
-        raise NotImplementedError("The fit method is not implemented yet.")
+        # Initialize weights and bias
+        if (X[0] is list):
+            self.weights = np.zeros(X.shape[1])
+        else:
+            self.weights = 0
+        self.bias = 0
+
+        for _ in range(self.epochs):
+            #lin_model = np.matmul(self.weights, X.transpose()) + self.bias
+            lin_model = self.weights * X + self.bias
+            grad_w, grad_b = self.compute_gradients(X, y, lin_model)
+            self.update_parameters(grad_w, grad_b)
     
     def predict(self, X):
         """
@@ -32,5 +45,17 @@ class LinearRegression():
         Returns:
             A length m array of floats
         """
-        # TODO: Implement
-        raise NotImplementedError("The predict method is not implemented yet.")
+        
+        # lin_model = np.matmul(X, self.weights) + self.bias
+        lin_model = self.weights * X + self.bias
+
+        return lin_model
+
+    def compute_gradients(self, X, y, lin_model):
+        w_derivative = -2/len(X) * np.sum((y - lin_model) * X)
+        b_derivative = -2/len(X) * np.sum(y - lin_model)
+        return w_derivative, b_derivative
+    
+    def update_parameters(self, grad_w, grad_b):
+        self.weights -= self.lr * grad_w
+        self.weights -= self.lr * grad_b
